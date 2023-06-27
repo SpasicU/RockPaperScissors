b26314d2e3a5e44b2fab1025f6a11389d2ec0526
Spasić Uroš <uros16419@its.edu.rs>
2023-06-27 13:12:41 +0200

    Made functions playRound and game, PvC mode.

--- skripta.js
+++ skripta.js
@@ -1,16 +1,70 @@
-function computerChoice(){
+function getComputerChoice(){
     let choice;
     let random = Math.floor(Math.random() * 3);
     if(random == 0)
     {
-        choice = "papir";
+        choice = "paper";
     }
     else if(random == 1){
-        choice = "kamen";
+        choice = "rock";
     }
     else {
-        choice = "makaze";
+        choice = "scissors";
     }
 
-    return console.log(choice);
+    return choice;
+}
+
+function playRound(playerChoice, computerSelection){
+    //let playerChoice = document.getElementById('playerSelection').value;
+    //let computerSelection = computerChoice();
+    //playRound(playerChoice, computerSelection);
+    
+    let p = playerChoice.toLowerCase();
+
+    if(p == "scissors")
+    {
+        if(computerSelection == "paper"){
+            return console.log("You Win! Scissors beat Paper!");
+        }
+        else if(computerSelection == "rock"){
+            return console.log("You Lose! Rock beats Scissors!");
+        }
+        else{
+            return console.log("Draw! You both picked scissors!");
+        }
+    }
+    else if(p == "rock")
+    {
+        if(computerSelection == "paper"){
+            return console.log("You Lose! Paper beats Rock!");
+        }
+        else if(computerSelection == "scissors"){
+            return console.log("You Win! Rock beats Scissors!");
+        }
+        else{
+            return console.log("Draw! You both picked rock!");
+        }
+    }
+    else if(p == "paper")
+    {
+        if(computerSelection == "paper"){
+            return console.log("Draw! You both picked paper!");
+        }
+        else if(computerSelection == "scissors"){
+            return console.log("You Lose! Scissors beat Paper!");
+        }
+        else{
+            return console.log("You win! Paper beats rock!");
+        }
+    }
+}
+
+function game(){
+    for(let i=0; i<3; i++){
+        console.log("Round number: " + (i+1))
+        let playerInput = prompt("Rock, Paper, Scissors?");
+        let computer = getComputerChoice();
+        playRound(playerInput, computer);
+    }
 }
