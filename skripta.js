63aedf753334715014d5ede5177faff383ae15f2
Spasić Uroš <uros16419@its.edu.rs>
2023-07-22 21:24:13 +0200

    Added buttons and UI

--- skripta.js
+++ skripta.js
@@ -1,3 +1,7 @@
+let rezComp = 0;
+let rezIgr = 0; 
+let rezultat = "";
+
 function getComputerChoice(){
     let choice;
     let random = Math.floor(Math.random() * 3);
@@ -15,56 +19,94 @@ function getComputerChoice(){
     return choice;
 }
 
-function playRound(playerChoice, computerSelection){
-    //let playerChoice = document.getElementById('playerSelection').value;
-    //let computerSelection = computerChoice();
-    //playRound(playerChoice, computerSelection);
-    
-    let p = playerChoice.toLowerCase();
+function disableButtons() {
+    document.querySelectorAll(".buttons").forEach(e => e.disabled = true);
+}
 
+function playRound(p, computerSelection){
+    
     if(p == "scissors")
     {
         if(computerSelection == "paper"){
-            return console.log("You Win! Scissors beat Paper!");
+            rezultat = "You Win! Scissors beat Paper!";
+            rezIgr+=1;
         }
         else if(computerSelection == "rock"){
-            return console.log("You Lose! Rock beats Scissors!");
+            rezultat = "You Lose! Rock beats Scissors!";
+            rezComp+=1;
         }
         else{
-            return console.log("Draw! You both picked scissors!");
+            rezultat = "Draw! You both picked Scissors!";
+            rezComp+=1;
         }
     }
     else if(p == "rock")
     {
         if(computerSelection == "paper"){
-            return console.log("You Lose! Paper beats Rock!");
+            rezultat = "You Lose! Paper beats Rock!";
+            rezComp+=1;
         }
         else if(computerSelection == "scissors"){
-            return console.log("You Win! Rock beats Scissors!");
+            rezultat = "You Win! Rock beats Scissors!";
+            rezIgr+=1;
         }
         else{
-            return console.log("Draw! You both picked rock!");
+            rezultat = "Draw! You both picked rock!";
         }
     }
     else if(p == "paper")
     {
         if(computerSelection == "paper"){
-            return console.log("Draw! You both picked paper!");
+            rezultat = "Draw! You both picked paper!";
         }
         else if(computerSelection == "scissors"){
-            return console.log("You Lose! Scissors beat Paper!");
+            rezultat = "You Lose! Scissors beat Paper!";
+            rezComp+=1;
         }
         else{
-            return console.log("You win! Paper beats rock!");
+            rezultat = "You win! Paper beats rock!";
+            rezIgr+=1;
         }
     }
+    return rezultat;
 }
 
-function game(){
-    for(let i=0; i<3; i++){
-        console.log("Round number: " + (i+1))
-        let playerInput = prompt("Rock, Paper, Scissors?");
-        let computer = getComputerChoice();
-        playRound(playerInput, computer);
+function game(playerChoice) {
+
+    let p = playerChoice.toLowerCase();
+    let computerSelection = getComputerChoice();
+  
+    let rezultatRunde = playRound(p, computerSelection);
+  
+    // if (rezultatRunde.search('You Win!') > -1) {
+    //     rezIgr++;
+    //  } else if (rezultatRunde.search('You Lose!') > -1) {
+    //     rezComp++;
+    //  }
+
+    document.getElementById("result").innerHTML = rezultatRunde;
+    document.getElementById("rezIgr").innerHTML = rezIgr;
+    document.getElementById("rezComp").innerHTML = rezComp;
+  
+    if (rezComp == 5) {
+        document.getElementById("result").innerHTML = 'Computer won! Reload the page to play again.';
+        disableButtons();
+    }else if (rezIgr == 5) {
+        document.getElementById("result").innerHTML = 'You won! Reload the page to play again.';
+        disableButtons();
     }
-}
+    
+  }
+
+
+
+// function game(){
+//     while(rezIgr<5 || rezComp<5) {
+//         console.log("Round number: " + (i+1))
+//         let playerInput = prompt("Rock, Paper, Scissors?");
+//         let computer = getComputerChoice();
+//         playRound(playerInput, computer);
+//         document.getElementById("rezIgr").innerHTML = " " + rezIgr;
+//         document.getElementById("rezComp").innerHTML = " " + rezComp;
+//     }
+// }
